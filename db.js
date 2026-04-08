f15ffc44c0d935dc480e7d951a678fa2faa0a14d
Gishard Raymond <raymondgishard@email.com>
2026-04-08 01:24:31 -0400

    fix variables for db

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE || 'fashionpalace',
-    port: process.env.MYSQLPORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:", err);
-    } else {
-        console.log("Connected to MySQL");
-    }
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
 });
 
 module.exports = db;
